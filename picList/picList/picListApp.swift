cd160a6ca95dd2d683a7cbfd79b5244493ab3b07
Gatien DIDRY <gatiendidry@MacBook-Pro-de-Gatien.local>
2022-04-06 09:55:36 +0200

    init project

---
+++ picList/picList/picListApp.swift
@@ -0,0 +1,20 @@
+//
+//  picListApp.swift
+//  picList
+//
+//  Created by Gatien DIDRY on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct picListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
